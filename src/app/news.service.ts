fc8d81c57e6791c83499e68632509594187e101f
Dmytro Abramov <dmytro.abramov@spryker.com>
2020-02-20 18:28:55 +0200

    homework-3

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -1,7 +1,7 @@
 // tslint:disable: variable-name
 import { Injectable } from '@angular/core';
-import { combineLatest, OperatorFunction, Subject } from 'rxjs';
-import { map, scan, shareReplay, startWith } from 'rxjs/operators';
+import { OperatorFunction, Subject, merge } from 'rxjs';
+import { scan, shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,16 +10,19 @@ export class NewsService {
   private _news$ = new Subject<string>();
   private _breakingNews$ = new Subject<string>();
 
-  news$ = this._news$.pipe(storeNews());
-  breakingNews$ = this._breakingNews$.pipe(storeNews());
+  news$ = this._news$.pipe(storeNews(), shareReplay());
+  breakingNews$ = this._breakingNews$.pipe(storeNews(), shareReplay());
 
-  allNews$ = combineLatest(
-    // combineLatest will start emitting when all of it's streams will emit at least one time
-    // that is why startWith() operator is required here - we want to emit normal news
-    // even when there were no breaking news yet and vice versa
-    this.breakingNews$.pipe(startWith([])),
-    this.news$.pipe(startWith([]))
-  ).pipe(map(([breaking, news]) => [...breaking, ...news]));
+  allNews$ = merge(   //merge two streams in one
+    this.breakingNews$.pipe(),
+    this.news$.pipe()
+  ).pipe(
+      scan((allNews, news) => { //as a second argument updated stream comes with 1 new value
+          const lastNews = news[news.length - 1];   //can it be removed somehow?
+          return [...allNews, lastNews];
+      }, []),
+      shareReplay()
+  );
 
   addNews(news: string) {
     this._news$.next(news);
