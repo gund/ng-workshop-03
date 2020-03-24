e8a8d77b3683243c5b58d62ea9ade3ac08691245
kishkinova <hanna.kishkinova@spryker.com>
2020-03-24 17:04:51 +0200

    HW-3 Fix bugs and improve rendering

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -1,41 +1,42 @@
 // tslint:disable: variable-name
 import { Injectable } from '@angular/core';
 import { combineLatest, OperatorFunction, Subject } from 'rxjs';
-import {map, scan, shareReplay, startWith, tap} from 'rxjs/operators';
+import {map, scan, shareReplay, startWith} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
-  private _news$ = new Subject<string>();
-  private _breakingNews$ = new Subject<string>();
+  private _news$ = new Subject<{title: string, date: number}>();
+  private _breakingNews$ = new Subject<{title: string, date: number}>();
 
   news$ = this._news$.pipe(storeNews(), shareReplay(1));
   breakingNews$ = this._breakingNews$.pipe(storeNews(), shareReplay(1));
 
   allNews$ = combineLatest(
-    // combineLatest will start emitting when all of it's streams will emit at least one time
-    // that is why startWith() operator is required here - we want to emit normal news
-    // even when there were no breaking news yet and vice versa
     this.breakingNews$.pipe(startWith([])),
     this.news$.pipe(startWith([]))
-  ).pipe(map(([breaking, news]) => [...breaking, ...news]));
+  ).pipe(
+    map(([breaking, news]) => {
+      return [...breaking, ...news].sort((a, b) => a.date - b.date);
+    }),
+  );
 
   addNews(news: string) {
-    this._news$.next(news);
+    this._news$.next({title: news, date: new Date().getTime()});
   }
 
   addBreakingNews(news: string) {
-    this._breakingNews$.next(`BREAKING: ${news}`);
+    this._breakingNews$.next({title: `BREAKING: ${news}`, date: new Date().getTime()});
   }
 }
 
 /**
  * RxJS operator function that accumulates every news into array of news
  */
-function storeNews(): OperatorFunction<string, string[]> {
+function storeNews(): OperatorFunction<object, object[]> {
   return news$ =>
     news$.pipe(
-      scan<string, string[]>((allNews, news) => [...allNews, news], [])
+      scan<object, object[]>((allNews, news) => [...allNews, news], [])
     );
 }
